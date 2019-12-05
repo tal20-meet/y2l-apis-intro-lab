db86fb2e384b15a5d5895e442bbdd8286ce85b6f
myname21-meet <myname21@meet.mit.edu>
2019-12-05 11:56:43 -0500

    finished

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def movies():
                     "image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
                     }
                     """
-    return render_template('movie.html', movie={})
+    parsed_json = json.loads(json_string)
+    return render_template('movie.html', movie=parsed_json)
 
 
 @app.route('/tvshows')
@@ -74,7 +75,10 @@ def dog_breeds():
     Do a GET request to the link above to get all dog breeds and return them
     to them as a list to the user as a bullet pointed list
     """
-    return render_template('dogs.html')
+    response = requests.get("https://dog.ceo/api/breeds/list/all")
+    parsed_content = json.loads(response.content)
+
+    return render_template('dogs.html', doggos = parsed_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
